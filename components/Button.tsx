e545c94d81aa8e8a2e5e2f8ce97a463bd748a277
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-27 21:53:30 +0530

    updated button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,10 +1,20 @@
+import Image from 'next/image'
 import React from 'react'
 
 function Button() {
   return (
-    <div className=''>
+    <div className='w-[8vw] h-[2vw] py-2 bg-red-200 rounded-full flex items-center justify-center'>
         <div className=''>
-            
+            <h5 className='text-[.7vw]'>Start a Project</h5>
+            <h5 className='text-[.7vw]'>Start a Project</h5>
+        </div>
+        <div>
+            <div>
+                <Image src='/icon.svg' width={8} height={8} alt="" />
+            </div>
+            <div>
+                <Image src='/icon.svg' width={8} height={8} alt="" />
+            </div>
         </div>
     </div>
   )
