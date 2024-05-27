e84d16ab98b6ea841c5ad9e1366c87b54ad7b86c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-27 22:39:10 +0530

    button

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,14 +3,14 @@ import React from 'react'
 
 function Button() {
   return (
-    <div className='px-[1vw] h-[2vw]  bg-red-200 rounded-full flex items-center justify-center'>
-        <div className='h-[1.4vw] overflow-hidden'>
+    <div className='px-[1vw] h-[2vw]  bg-white rounded-full flex items-center gap-[.5vw] justify-center'>
+        <div className='h-[1.4vw] overflow-hidden text-black '>
             <h5 className='text-[.8vw] '>Start a Project</h5>
             <h5 className='text-[.8vw]'>Start a Project</h5>
         </div>
-        <div className='h-[1.4vw] overflow-hidden'>
-            <Image src='/icon.svg' width={8} height={8} alt="" className='w-[1vw]' />
-            <Image src='/icon.svg' width={8} height={8} alt="" className='w-[1vw]' />
+        <div className='h-[.5vw] overflow-hidden'>
+            <Image src='/icon.svg' width={8} height={8} alt="" className='w-[.9vw]' />
+            <Image src='/icon.svg' width={8} height={8} alt="" className='w-[.9vw]' />
         </div>
     </div>
   )
