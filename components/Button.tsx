fc9cd1de62be84ee5e67a41e182a8124a459091b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-27 15:57:54 +0530

    first commit

---
+++ components/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Button() {
+  return (
+    <div className=''>
+        <div className=''>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Button
---
+++ components/Navbar.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='max-w-screen-xl mx-auto py-[1vw] flex items-center justify-center'>
+        <div className='flex items-center gap-[4vw]'>
+            <Image src='/logo.svg' width={32} height={32} alt="" className='w-[5vw]' />
+            <div className='flex items-center gap-[3vw]'>
+                {["Home", "Work", "About", "News", "Careers"].map((item, index) => {
+                    return (
+                        <div key={index} className=''>
+                            <Link href="/">
+                                <p className='text-[.8vw] flex items-center gap-[.2vw]'>
+                                    {index === 1 && <span className='inline-block w-[.2vw] h-[.2vw] rounded-full bg-green-500'></span> }
+                                    {item}
+                                </p>
+                            </Link>  
+                        </div>
+                    )
+                })}
+            </div>   
+        </div>
+
+      
+    </div>
+  )
+}
+
+export default Navbar
