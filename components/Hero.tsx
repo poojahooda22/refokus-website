37443bbae9599f6932d79ac2434b02a6076e5dd8
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-27 21:53:37 +0530

    created hero

---
+++ components/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='max-w-screen-xl mx-auto '>
+      <div className='text-[22vw] text-center '>work</div>
+    </div>
+  )
+}
+
+export default Hero
