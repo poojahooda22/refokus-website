1abd9dc3b5af53c7214a280b77bc7fc5c7bca479
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-27 22:56:53 +0530

    updated navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import Button from './Button'
+import Button from './button/Button'
 
 function Navbar() {
   return (
