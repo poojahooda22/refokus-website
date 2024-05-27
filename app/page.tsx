0a6093907721dab0e85f48385e1c2fddf2bb1699
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-27 21:53:23 +0530

    updated page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <main className="w-full h-screen bg-zinc-900 text-white">
+    <main className="w-full h-screen bg-zinc-900 text-white font-[satoshi_variable]">
       <Navbar />
+      <Hero />
     </main>
   );
 }
