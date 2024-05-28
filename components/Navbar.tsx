9b42286fd68067d9ef327da0355a59f5d5c0590c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-28 17:17:39 +0530

    updated navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,29 +2,12 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import Button from './button/Button'
+import { RiMenu3Fill } from "react-icons/ri";
 
 function Navbar() {
   return (
-    <div className='max-w-screen-xl mx-auto py-[1vw] flex items-center justify-between px-[4vw]'>
-        <div className='flex items-center gap-[4vw]'>
-            <Image src='/logo.svg' width={32} height={32} alt="" className='w-[5vw]' />
-            <div className='flex items-center gap-[3vw]'>
-                {["Home", "Work", "About", "News", "Careers"].map((item, index) => {
-                    return (
-                        <div key={index} className=''>
-                            <Link href="/">
-                                <p className='text-[.8vw] flex items-center gap-[.2vw]'>
-                                    {index === 1 && <span style={{boxShadow:'0 0 0.25em #00FF19'}} className='inline-block w-[.2vw] h-[.2vw] rounded-full bg-green-500'></span> }
-                                    {item}
-                                </p>
-                            </Link>  
-                        </div>
-                    )
-                })}
-            </div>   
-        </div>
-
-        <Button />  
+    <div className=''>
+                 
     </div>
   )
 }
