754c302e40180107f316735cf17b24de11eff5e4
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-28 23:05:12 +0530

    updated index

--- components/header/navbar/Index.tsx
+++ components/header/navbar/Index.tsx
@@ -2,13 +2,14 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import Button from '../../button/Button'
+import {motion} from 'framer-motion'
 
 
 function Navbar() {
   return (
-    <div className=''>
-             Hello    
-    </div>
+    <motion.div className='bg-secondary w-full h-[100vh] text-primary'>
+      Hello    
+    </motion.div>
   )
 }
 
