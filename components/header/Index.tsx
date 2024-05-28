f8945330be8a95f6a58b0af01cf464ab5e577745
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-28 22:41:23 +0530

    updated index

--- components/header/Index.tsx
+++ components/header/Index.tsx
@@ -13,7 +13,7 @@ function Header() {
     const [isActive, setIsActive] = React.useState(false)
 
     return (
-        <div className='max-w-screen-2xl mx-auto sm:py-[1vw] flex items-center justify-between py-[6vw] px-[5vw] sm:px-0 '>
+        <div className='max-w-screen-2xl mx-auto  sm:py-[1vw] flex items-center justify-between py-[6vw] px-[5vw] sm:px-0 '>
             <div className='flex items-center gap-[4vw]'>
                 <Image src='/logo.svg' width={32} height={32} alt="" className='w-[20vw] sm:w-[5vw]' />
                 <div className='hidden sm:flex items-center gap-[3vw]'>
@@ -39,11 +39,13 @@ function Header() {
             <motion.div
                 animate={!isActive ? "closed" : "open"}
                 onMouseDown={() => {setIsActive(!isActive)}}
+                className='flex items-center justify-center sm:hidden cursor-pointer relative '
                     
             >
                 <motion.h3 
                     variants={opacity} 
                     animate={isActive ? "closed" : "open"}
+                    className=''
                 >
                     <HiOutlineMenuAlt4 size={28} />
                 </motion.h3>
