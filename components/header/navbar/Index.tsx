cb28a46a11b6ab51734e5d3da360a1debd1cef57
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-28 22:55:59 +0530

    created index

---
+++ components/header/navbar/Index.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import Button from '../../button/Button'
+
+
+function Navbar() {
+  return (
+    <div className=''>
+                 
+    </div>
+  )
+}
+
+export default Navbar
