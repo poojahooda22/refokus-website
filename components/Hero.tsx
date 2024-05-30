fd9904e117e32c132de6e1048491aa5374ef4fdd
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-30 15:31:56 +0530

    hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Image from 'next/image'
 
 function Hero() {
   return (
-    <div className=''>
-      <div className=''></div>
+    <div className='w-full flex  items-center justify-center'>
+      <div className='flex bg-comp mt-[12vw]'>
+        <div className='text-[8vw]'>
+          <div><h1>We</h1></div>
+          <div><h1>Make</h1></div>
+          <div><h1>Cool</h1></div>
+          <div><h1>Websites</h1></div>
+        </div>
+        <div>
+          <Image src='/iconLogo.svg' width={32} height={32} alt="" />
+        </div>
+      </div>
     </div>
   )
 }
