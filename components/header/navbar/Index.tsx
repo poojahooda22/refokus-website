9d46d86cf5891b775d70aba9a7ca91c2b993ca71
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-30 15:30:11 +0530

    updated index

--- components/header/navbar/Index.tsx
+++ components/header/navbar/Index.tsx
@@ -1,48 +1,53 @@
 'use client'
 
-import React, { useState } from 'react'
-import Link from 'next/link'
-import Button from '../../button/Button'
-import {motion} from 'framer-motion'
-import { menuSlide } from '../anime';
+import React, {useState} from 'react';
+import { motion } from 'framer-motion';
 import { usePathname } from 'next/navigation';
+import { menuSlide } from '../anime'
+import styles from './style.module.scss';
+import Link from './link/Index'
+import Curve from './Curve/Index'
 
 const navItems = [
-  {
-    title: "Home",
-    href: "/",
-  },
-  {
-    title: "Work",
-    href: "/work",
-  },
-  {
-    title: "About",
-    href: "/about",
-  },
-  {
-    title: "Contact",
-    href: "/contact",
-  },
-]
+    {
+      title: "Home",
+      href: "/",
+    },
+    {
+      title: "Work",
+      href: "/work",
+    },
+    {
+      title: "About",
+      href: "/about",
+    },
+    {
+      title: "Contact",
+      href: "/contact",
+    },
+  ]
 
+function Nav() {
+    const pathname = usePathname();
+    const [selectedIndicator, setSelectedIndicator] = useState(pathname);
 
-function Navbar() {
-  const pathname = usePathname();
-  const [selectedIndicator, setSelectedIndicator] = useState(pathname);
   return (
-    <motion.div 
-      initial="initial" animate="enter" exit="exit" 
-      className='bg-secondary w-full h-[100vh] text-primary '
-    >
-      {
-        navItems.map( (data, index) => {
-          return (
-            <p key={index}>{data.title}</p>
-          )
-      })    }
+    <motion.div variants={menuSlide} initial="initial" animate="enter" exit="exit" className={styles.menu}>
+       <div className={styles.body}>
+            <div onMouseLeave={() => {setSelectedIndicator(pathname)}} className={styles.nav}>
+                    <div className={styles.header}>   
+                    </div>
+                    {
+                      navItems.map( (data, index) => {
+                        return <Link key={index} data={{...data, index}} isActive={selectedIndicator == data.href} setSelectedIndicator={setSelectedIndicator}></Link>
+                      })
+                    }
+            </div>
+            {/* <Footer /> */}
+        </div>
+        <Curve />
     </motion.div>
   )
 }
 
-export default Navbar
+export default Nav
