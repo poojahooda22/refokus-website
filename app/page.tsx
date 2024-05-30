53f9dbadda39ba218fc2e50ddca69fcdaaf951f7
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-30 15:04:00 +0530

    updated page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
+
+'use client'
 import Hero from "@/components/Hero";
-import Header from "@/components/header/Index"
+import Cursor from "@/components/StickyCursor/Index";
+import Header from "@/components/header/Index";
+import { useRef,  useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    (
+      async () => {
+        const LocomotiveScroll = (await import ('locomotive-scroll')).default;
+        const locomotiveScroll = new LocomotiveScroll();
+      }
+    )()
+  }, [])
+
+  const stickyElement = useRef(null);
   return (
     <main className="w-full h-screen bg-primary text-secondary font-[satoshi_variable]">
-      <Header/>
+      <Header ref={stickyElement} />
+      <Cursor stickyElement={stickyElement} />
       <Hero />
     </main>
   );
