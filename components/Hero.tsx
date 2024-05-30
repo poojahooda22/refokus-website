d2302657f786aed414e10662edbbd186934bb258
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-30 23:14:54 +0530

    updated hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,17 +8,18 @@ function Hero() {
         <div className='flex items-end justify-end '>
           <div className='w-1/2 h-[4vw]'></div>
           <div className='w-[8vw] h-[12vw] sm:w-[3vw] sm:h-[2vw] flex items-center'>
-            <Image src='/iconLogo.svg' width={32} height={32} alt="" className='w-[5vw] lg:w-[2vw] xl:w-[1.5vw]' />
+            <Image src='/iconLogo.svg' width={32} height={32} alt="" 
+            className='w-[5vw] lg:w-[2vw] xl:w-[1.5vw]' />
           </div>
         </div>
         <div className='sm:flex items-end justify-between'>
-          <div className='text-[12vw] leading-[11vw] sm:text-[6vw] sm:leading-[5vw] uppercase'>
+          <div className='text-[12vw] leading-[11vw] sm:text-[6vw] sm:leading-[5vw] lg:text-[9vw] lg:leading-[8vw] uppercase tracking-tighter'>
             <h1>We</h1>
             <h1>make</h1>
             <h1>cool</h1>
             <h1>websites</h1>
           </div>
-          <div className='sm:w-1/3 sm:text-[1vw] text-[4vw] mt-[8vw] sm:mt-0'>
+          <div className='sm:w-1/3 sm:text-[1vw] text-[4vw] mt-[8vw] sm:mt-0 lg:text-[1.2vw]'>
             <p>Refokus is a forward-thinking web agency that combines Web Design, Webflow Development, and Creative Development to create cool websites that help brands position themselves as market leaders.</p>
           </div>
         </div>  
