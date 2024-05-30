0a91b4a1118c36ec6acc5c99a3fdf84968a71468
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-30 21:56:32 +0530

    updated hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,21 +4,21 @@ import Image from 'next/image'
 function Hero() {
   return (
     <div className='w-screen relative pt-[8vw]'>
-      <div className=' max-w-screen-xl mx-auto flex flex-col gap-[8vw]'>
+      <div className=' max-w-screen-xl mx-auto flex flex-col gap-[8vw] px-[5vw] xl:px-0 mt-[20vw] lg:mt-[8vw] xl:mt-0'>
         <div className='flex items-end justify-end '>
           <div className='w-1/2 h-[4vw]'></div>
-          <div className='w-[4vw] h-[2vw] flex items-center'>
+          <div className='w-[8vw] h-[12vw] sm:w-[4vw] sm:h-[2vw] flex items-center'>
             <Image src='/iconLogo.svg' width={32} height={32} alt="" className='w-[5vw] sm:w-[1.5vw]' />
           </div>
         </div>
-        <div className='flex items-end justify-between'>
-          <div className='text-[6vw] leading-[5vw] uppercase'>
+        <div className='sm:flex items-end justify-between'>
+          <div className='text-[12vw] leading-[11vw] sm:text-[6vw] sm:leading-[5vw] uppercase'>
             <h1>We</h1>
             <h1>make</h1>
             <h1>cool</h1>
             <h1>websites</h1>
           </div>
-          <div className='w-1/3 text-[1vw]'>
+          <div className='sm:w-1/3 sm:text-[1vw] text-[4vw] mt-[8vw] sm:mt-0'>
             <p>Refokus is a forward-thinking web agency that combines Web Design, Webflow Development, and Creative Development to create cool websites that help brands position themselves as market leaders.</p>
           </div>
         </div>  
