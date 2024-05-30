c592dff31c5f60f4b077c3b185c628075636054f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-30 20:44:49 +0530

    updated index

--- components/header/Index.tsx
+++ components/header/Index.tsx
@@ -23,8 +23,8 @@ const Header = forwardRef(function Index(props, ref){
     }, [pathname])
 
     return (
-        <>
-            <div className={styles.header} style={{ overflow: "hidden"}}>
+        <div className='w-screen flex  justify-center'>
+            <div className={`${styles.header} max-w-screen-xl mx-auto py-[1vw]`} style={{ overflow: "hidden"}}>
                 <div className='flex items-center gap-[4vw]'>
                     <Image src='/logo.svg' width={32} height={32} alt="" className='w-[20vw] sm:w-[5vw]' />
                     <div className='hidden sm:flex items-center gap-[3vw]'>
@@ -55,7 +55,7 @@ const Header = forwardRef(function Index(props, ref){
             <AnimatePresence mode="wait">
                 {isActive && <Navbar />}
             </AnimatePresence>
-        </>
+        </div>
     )
 })
 
