5bf76793b31d25529069b40429db0ca1a9ec9647
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-30 22:40:26 +0530

    updated hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,11 +4,11 @@ import Image from 'next/image'
 function Hero() {
   return (
     <div className='w-screen relative pt-[8vw]'>
-      <div className=' max-w-screen-xl mx-auto flex flex-col gap-[8vw] px-[5vw] xl:px-0 mt-[20vw] lg:mt-[8vw] xl:mt-0'>
+      <div className=' max-w-screen-xl mx-auto flex flex-col gap-[14vw] px-[5vw] xl:px-0 mt-[20vw] lg:mt-[8vw] xl:mt-0'>
         <div className='flex items-end justify-end '>
           <div className='w-1/2 h-[4vw]'></div>
-          <div className='w-[8vw] h-[12vw] sm:w-[4vw] sm:h-[2vw] flex items-center'>
-            <Image src='/iconLogo.svg' width={32} height={32} alt="" className='w-[5vw] sm:w-[1.5vw]' />
+          <div className='w-[8vw] h-[12vw] sm:w-[3vw] sm:h-[2vw] flex items-center'>
+            <Image src='/iconLogo.svg' width={32} height={32} alt="" className='w-[5vw] lg:w-[2vw] xl:w-[1.5vw]' />
           </div>
         </div>
         <div className='sm:flex items-end justify-between'>
