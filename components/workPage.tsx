6c04ba434cf8a3ad1e02041fae77769cd803b3c6
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-03 23:21:07 +0530

    updated data

--- components/workPage.tsx
+++ components/workPage.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function WorkPage() {
   const images = [
     {
-      url: "",
+      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef09178195ce0073e38f3_Refokus%20Tools-1.png",
       top: 0,
       left: 0,
       isActive: false
