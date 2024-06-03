f46a11ce632bd4db513191b5d67ec19b9cf12ebf
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-03 21:11:37 +0530

    updated page

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,11 +1,12 @@
 import Header from '@/components/header/Index'
 import React from 'react'
+import WorkPage from '@/components/workPage'
 
 function Work() {
   return (
     <div className='w-screen h-[100vh] relative bg-primary text-secondary'>
         <Header/>
-        
+        <WorkPage/>
     </div>
   )
 }
