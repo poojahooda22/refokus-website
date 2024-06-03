10c8d6fa5c9dd8fc7de7c5c1fe1426d1e45dc6f6
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-03 23:00:30 +0530

    updated index

--- components/StickyCursor/Index.tsx
+++ components/StickyCursor/Index.tsx
@@ -59,7 +59,7 @@ export default function Index({stickyElement}: any) {
    
   }
 
-  const manageMouseOver = () => {
+  const manageMouseOver = e => {
     setIsHovered(true);
   }
   const manageMouseLeave = e => {
@@ -68,15 +68,15 @@ export default function Index({stickyElement}: any) {
   }
 
   useEffect( () => {
-    window.addEventListener("mousemove", manageMouseMove)
-    stickyElement.current.addEventListener("mouseover", manageMouseOver)
+    stickyElement.current.addEventListener("mouseenter", manageMouseOver)
     stickyElement.current.addEventListener("mouseleave", manageMouseLeave)
+    window.addEventListener("mousemove", manageMouseMove);
     return () => {
-      window.removeEventListener("mousemove", manageMouseMove)
-      stickyElement.current.removeEventListener("mouseover", manageMouseOver)
+      stickyElement.current.removeEventListener("mouseenter", manageMouseOver)
       stickyElement.current.removeEventListener("mouseleave", manageMouseLeave)
+      window.removeEventListener("mousemove", manageMouseMove)
     }
-  })
+  }, [isHovered])
 
   const template = ({rotate, scaleX, scaleY}: any) => {
     return `rotate(${rotate}) scaleX(${scaleX}) scaleY(${scaleY})` 
@@ -95,7 +95,6 @@ export default function Index({stickyElement}: any) {
       }}
       animate={{width: cursorSize, height: cursorSize}}
     >
-    
     </motion.div>
   )
 }
