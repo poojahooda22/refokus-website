6fe771f932b17a2899dcea39b80e9688bcd3e9bf
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-03 23:26:23 +0530

    updated page

--- components/workPage.tsx
+++ components/workPage.tsx
@@ -21,7 +21,13 @@ function WorkPage() {
       left: 0,
       isActive: false
     },
-    
+    {
+      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0acbc45cb2f4fc5c6b2_Yahoo.png",
+      top: 0,
+      left: 0,
+      isActive: false
+    },
+
   ]
 
   return (
