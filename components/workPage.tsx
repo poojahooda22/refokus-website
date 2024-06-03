3e187bfcd0e746f2bb30fb67fd752664bd657298
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-03 23:21:33 +0530

    updated page

--- components/workPage.tsx
+++ components/workPage.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 
 function WorkPage() {
+  
   const images = [
     {
       url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef09178195ce0073e38f3_Refokus%20Tools-1.png",
       top: 0,
       left: 0,
       isActive: false
-    }
+    },
+
   ]
+
   return (
     <div className='w-full'>
         <div className='max-w-screen-xl mx-auto pt-[8vw] text-center'>
