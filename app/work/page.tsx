5e044f044025fa4a53f37847689bfd9b32e0b371
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-03 15:58:16 +0530

    updated page

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,9 +1,13 @@
+import Header from '@/components/header/Index'
 import React from 'react'
 
 function Work() {
   return (
-    <div className=''>
-      <div className=''><h1>Coming Soon...</h1></div>
+    <div className='w-screen h-[100vh] relative bg-primary text-secondary'>
+        <Header/>
+        <div className=''>
+            <h1>Coming Soon...</h1>
+        </div>
     </div>
   )
 }
