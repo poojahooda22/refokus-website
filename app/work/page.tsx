6fdb03623375671774fa73500cf07a93e8b45a98
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-03 21:30:36 +0530

    updated page

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,10 +1,16 @@
+'use client'
+
 import Header from '@/components/header/Index'
 import React from 'react'
 import WorkPage from '@/components/workPage'
+import Cursor from "@/components/StickyCursor/Index";
+import { useRef} from 'react';
 
 function Work() {
+    const stickyElement = useRef(null);
   return (
     <div className='w-screen h-[100vh] relative bg-primary text-secondary'>
+        <Cursor stickyElement={stickyElement} />
         <Header/>
         <WorkPage/>
     </div>
