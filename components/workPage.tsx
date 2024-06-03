62735b48c32331c5e9a4e42bcad2e4a806ba155a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-03 23:21:47 +0530

    updated

--- components/workPage.tsx
+++ components/workPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function WorkPage() {
-  
+
   const images = [
     {
       url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef09178195ce0073e38f3_Refokus%20Tools-1.png",
@@ -9,7 +9,12 @@ function WorkPage() {
       left: 0,
       isActive: false
     },
-
+    {
+      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef09178195ce0073e38f3_Refokus%20Tools-1.png",
+      top: 0,
+      left: 0,
+      isActive: false
+    }
   ]
 
   return (
