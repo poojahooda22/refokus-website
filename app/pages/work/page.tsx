e7e0cce1b50e437068b1f8c0055504651fe28234
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-03 13:55:16 +0530

    updated page

--- app/pages/work/page.tsx
+++ app/pages/work/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function page() {
+function Work() {
   return (
-    <div>
-      Coming soon
+    <div className='w-full h-[100vh] text-white flex items-center justify-center '>
+      <div className='sm:text-[4vw]'><h1>Coming Soon...</h1></div>
     </div>
   )
 }
 
-export default page
+export default Work
