30cb3d01d13a21e1abccd8d428ae13e6899b4fde
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-03 20:09:25 +0530

    update workpage

--- components/workPage.tsx
+++ components/workPage.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 function workPage() {
   return (
     <div className='w-full'>
-        
+        <div className='max-w-screen-xl mx-auto pt-[8vw] text-center'>
+            <h1 className='lg:text-[28vw] font-medium leading-none'>work</h1>
+        </div>
     </div>
   )
 }
