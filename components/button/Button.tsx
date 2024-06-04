7ac7a3a84f6527070d5716921bc6ca37fc01215c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-04 23:24:01 +0530

    added

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -7,7 +7,7 @@ function Button() {
       <div className='h-[1.4vw] ctaWrapper overflow-hidden text-primary '>
             <h5 className='text-[.8vw] '>Start a Project</h5>
             <h5 className='text-[.8vw]'>Start a Project</h5>
-        </div>
+      </div>
         <div className='h-[.5vw] ctaIcon overflow-hidden'>
             <Image src='/icon.svg' width={8} height={8} alt="" className='w-[.9vw]' />
             <Image src='/icon.svg' width={8} height={8} alt="" className='w-[.9vw]' />
