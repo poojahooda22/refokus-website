cded6f6a5ef1e61dbf471f4660fb86c007519841
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-04 23:20:14 +0530

    updated hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,8 @@ import Image from 'next/image'
 function Hero() {
   return (
     <div className='w-screen relative pt-[8vw]'>
-      <div className=' max-w-screen-xl mx-auto flex flex-col gap-[10vw] px-[5vw] xl:px-0 mt-[20vw] sm:mt-[6vw] lg:mt-[6vw] xl:mt-0'>
+      <div className=' max-w-screen-xl mx-auto 
+      flex flex-col gap-[10vw] px-[5vw] xl:px-0 mt-[20vw] sm:mt-[6vw] lg:mt-[6vw] xl:mt-0'>
         <div className='flex items-end justify-end '>
           <div className='w-1/2 h-[4vw]'></div>
           <div className='w-[8vw] h-[12vw] sm:w-[3vw] 
