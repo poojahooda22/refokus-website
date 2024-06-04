8995a57bc47d8409ae9bca1bfc62e17fd7b6c47e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-04 23:21:15 +0530

    updated hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,7 @@ function Hero() {
         flex flex-col gap-[10vw] px-[5vw] xl:px-0 mt-[20vw] 
         sm:mt-[6vw] lg:mt-[6vw] xl:mt-0'
       >
-        <div className='flex items-end justify-end '>
+        <div className='flex items-end justify-end'>
           <div className='w-1/2 h-[4vw]'></div>
           <div className='w-[8vw] h-[12vw] sm:w-[3vw] 
             sm:h-[2vw] flex items-center'
