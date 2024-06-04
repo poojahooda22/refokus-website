e323cedd215cd3e26b0eb4338a6a244cb0bd2d12
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-04 23:28:13 +0530

    added

--- components/header/Index.tsx
+++ components/header/Index.tsx
@@ -52,7 +52,7 @@ const Header = forwardRef(function Index(props, ref){
                                                 <span 
                                                     style={{boxShadow:'0 0 0.25em #00FF19'}} 
                                                     className='inline-block w-[.3vw] h-[.3vw] rounded-full bg-comp'
-                                                    >
+                                                >
                                                         </span>}
                                             {item.title}
                                         </p>
