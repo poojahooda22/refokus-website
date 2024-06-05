fab88c8259de129010bb9de21d3ee1d87df5a73d
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-05 23:02:04 +0530

    updated button

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -1,12 +1,12 @@
 import Image from 'next/image'
 import React from 'react'
 
-function Button() {
+function Button({text}: {text: string}) {
   return (
-    <div className='px-[1vw] h-[2vw]  bg-secondary rounded-full flex items-center gap-[.5vw] justify-center'>
+    <div className='w-fit px-[1vw] h-[2vw]  bg-secondary rounded-full flex items-center gap-[.5vw] justify-center'>
       <div className='h-[1.4vw] ctaWrapper overflow-hidden text-primary '>
-        <h5 className='text-[.8vw] '>Start a Project</h5>
-        <h5 className='text-[.8vw]'>Start a Project</h5>
+        <h5 className='text-[.8vw] '>{text}</h5>
+        <h5 className='text-[.8vw]'>{text}</h5>
       </div>
       <div className='h-[.5vw] ctaIcon overflow-hidden'>
         <Image src='/icon.svg' width={8} height={8} alt="" className='w-[.9vw]' />
