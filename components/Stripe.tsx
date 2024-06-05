045b19f152a1cb817529762e6ea2c6aa86ab0f05
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-05 21:19:33 +0530

    updated stripe

--- components/Stripe.tsx
+++ components/Stripe.tsx
@@ -4,7 +4,7 @@ function Stripe({val}: {val: any}) {
     return (
         <div 
             className='relative w-[50%] sm:w-[16.66%] sm:px-[1vw] px-[4vw] py-[2vw] sm:py-[1vw] border-r-[1px] border-t-[1px]
-                border-b-[1px] border-[#333] flex justify-between items-center'
+            border-b-[1px] border-[#333] flex justify-between items-center'
         >
             <img src={val.url} alt="" className='w-[40vw] sm:w-[7vw]'/>
             <span className='text-[4vw] sm:text-[1vw] font-semibold'>{val.number}</span>
