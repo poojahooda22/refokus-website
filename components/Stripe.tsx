9a3bcedeffe2196bd57e7aa3b3d501cc450a5add
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-05 16:56:50 +0530

    updated stripe

--- components/Stripe.tsx
+++ components/Stripe.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-function Stripe({val}) {
-  return (
-    <div 
-        className='relative max-w-72 px-[1vw] py-[1vw] border-r-[1px] border-t-[1px] 
-        border-b-[1px] border-[#333] flex justify-between items-center'
-    >
-      <img src={val.url} alt="" className='w-[4vw]'/>
-      <span className='font-semibold'>{val.number}</span>
-    </div>
-  )
+function Stripe({val}: {val: any}) {
+    return (
+        <div 
+                className='relative w-[50%] sm:w-[16.66%] sm:px-[1vw] px-[4vw] py-[2vw] sm:py-[1vw] border-r-[1px] border-t-[1px]
+                border-b-[1px] border-[#333] flex justify-between items-center'
+        >
+            <img src={val.url} alt="" className='w-[40vw] sm:w-[7vw]'/>
+            <span className='text-[4vw] sm:text-[1vw] font-semibold'>{val.number}</span>
+        </div>
+    )
 }
 
 export default Stripe
