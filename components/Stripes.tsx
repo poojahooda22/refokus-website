a4e6c5ac67ad7b91c0bdbc692b08177b13a0f052
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-05 15:20:39 +0530

    updated stripes

--- components/Stripes.tsx
+++ components/Stripes.tsx
@@ -4,10 +4,9 @@ import Stripe from './Stripe'
 function Stripes() {
     var data = [
         {
-            url: "",
+            url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63c9ce90a51cd75d563689fd_Awwards-logotype-2018%201.svg",
             number: "",
-            
-
+        
         }
     ]
   return (
