deed37645e92a55a00b348398b27856458199e40
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-05 15:47:26 +0530

    updated stripe

--- components/Stripe.tsx
+++ components/Stripe.tsx
@@ -4,7 +4,8 @@ function Stripe() {
   return (
     <div 
         className='max-w-72 px-4 py-3 border-r-[1px] border-t-[1px] 
-    border-b-[1px] border-[#333] flex justify-between items-center'>
+        border-b-[1px] border-[#333] flex justify-between items-center'
+        >
       <span>image</span>
       <span>43</span>
     </div>
