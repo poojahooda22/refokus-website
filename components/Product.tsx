50905e05d3db781ef9168a56d01feb19c7a4f046
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-05 23:02:12 +0530

    updated product

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import Button from './button/Button'
 
 function Product() {
   return (
     <div className='w-full py-5'>
-      <div className='max-w-screen-xl mx-auto'>
-       <h2>Arqitel</h2>
-       <div>
-        
+      <div className='max-w-screen-xl mx-auto flex justify-between '>
+       <h2 className='text-[2vw] font-medium'>Arqitel</h2>
+       <div className='w-1/3'>
+            <p>
+                With a continuous 3D animation, we showcase Arqitel approach and 
+                show how migration data translates into real estate.
+            </p>
+            <Button text='Live Website' />
        </div>
       </div>
     </div>
