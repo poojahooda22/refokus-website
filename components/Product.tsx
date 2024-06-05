7d34928af2c2ac9466c048165f985d2ad267b30c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-05 22:50:47 +0530

    updated product

--- components/Product.tsx
+++ components/Product.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 function Product() {
   return (
     <div className='w-full py-5'>
-      
+      <div className='max-w-screen-xl mx-auto'>
+        Arqitel
+      </div>
     </div>
   )
 }
