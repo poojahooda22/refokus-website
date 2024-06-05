8559a091ad70f9dfccafa539af4d2179df76879a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-05 21:19:23 +0530

    updated stripe

--- components/Stripe.tsx
+++ components/Stripe.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Stripe({val}: {val: any}) {
     return (
         <div 
-                className='relative w-[50%] sm:w-[16.66%] sm:px-[1vw] px-[4vw] py-[2vw] sm:py-[1vw] border-r-[1px] border-t-[1px]
+            className='relative w-[50%] sm:w-[16.66%] sm:px-[1vw] px-[4vw] py-[2vw] sm:py-[1vw] border-r-[1px] border-t-[1px]
                 border-b-[1px] border-[#333] flex justify-between items-center'
         >
             <img src={val.url} alt="" className='w-[40vw] sm:w-[7vw]'/>
