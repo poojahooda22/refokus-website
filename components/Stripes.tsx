565e37b7bd1b747d53a2356fa274e147b5d9ad12
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-05 15:21:16 +0530

    updated stripes

--- components/Stripes.tsx
+++ components/Stripes.tsx
@@ -6,7 +6,6 @@ function Stripes() {
         {
             url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63c9ce90a51cd75d563689fd_Awwards-logotype-2018%201.svg",
             number: "15",
-        
         }
     ]
   return (
