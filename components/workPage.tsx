c811f899b74ad861992326b4633947b76ec44db6
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-05 12:15:37 +0530

    updated page

--- components/workPage.tsx
+++ components/workPage.tsx
@@ -5,14 +5,14 @@ function WorkPage() {
   const images = [
     {
       url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef09178195ce0073e38f3_Refokus%20Tools-1.png",
-      top: "50%",
+      top: "60%",
       left: "50%",
       isActive: false
     },
     {
       url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0accfe1b3e66bc55462_Refokus%20Tools.png",
-      top: "53%",
-      left: "48%",
+      top: "56%",
+      left: "45%",
       isActive: false
     },
     {
@@ -23,14 +23,14 @@ function WorkPage() {
     },
     {
       url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0ac7e7179d210dc41f0_Summon.png",
-      top: "51%",
+      top: "60%",
       left: "49%",
       isActive: false
     },
     {
       url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0af108a465002975acd_Showcase%20Websites%20(1).png",
       top: "53%",
-      left: "53%",
+      left: "58%",
       isActive: false
     },
 
@@ -44,7 +44,7 @@ function WorkPage() {
                 {images.map((elem, index) => {
                   return (
                     <img key={index} src={elem.url} alt=""
-                      className='absolute w-[16vw] rounded-lg -translate-x-1/2'
+                      className='absolute w-[16vw] rounded-lg -translate-x-1/2 -translate-y-1/2'
                       style={{top: elem.top, left: elem.left}}
                     />
                   )
