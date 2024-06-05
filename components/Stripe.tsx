3ab191aa44bb5386e795ac30126a270861224883
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-05 16:11:12 +0530

    updated stripe

--- components/Stripe.tsx
+++ components/Stripe.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-function Stripe() {
+function Stripe({val}) {
   return (
     <div 
-        className='relative max-w-72 px-4 py-3 border-r-[1px] border-t-[1px] -mt-[4vw]
+        className='relative max-w-72 px-[1vw] py-[1vw] border-r-[1px] border-t-[1px] -mt-[4vw]
         border-b-[1px] border-[#333] flex justify-between items-center'
     >
-      <span>image</span>
-      <span>43</span>
+      <img src={val.url} alt="" className='w-[4vw]'/>
+      <span className='font-semibold'>{val.number}</span>
     </div>
   )
 }
