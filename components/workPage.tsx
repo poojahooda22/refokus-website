378e509408e2e158b2b9125afdbfcd9c66a45840
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-05 22:19:46 +0530

    updated work

--- components/workPage.tsx
+++ components/workPage.tsx
@@ -54,7 +54,7 @@ function WorkPage() {
                     />
                   )
                 )
-                )}
+              )}
             </div>
             
             <p className='hidden sm:inline-block text-[4.2vw] sm:text-[1.5vw] xl:text-[1vw] font-regular text-[#787878]'>Web Design, Webflow Development, Creative Development</p>   
