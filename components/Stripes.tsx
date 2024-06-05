78929f74bbe77cc00205cc79b575264c4f9940e5
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-05 16:43:26 +0530

    updated stripe

--- components/Stripes.tsx
+++ components/Stripes.tsx
@@ -15,7 +15,19 @@ function Stripes() {
             url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63d6e83420934a94d642103b_NCC2021_LogoLockup%201.svg",
             number: "2",
         },
-         
+        {
+            url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63c9ce90a51cd75d563689fd_Awwards-logotype-2018%201.svg",
+            number: "15",
+        },
+        {
+            url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63c9ce90a51cd7cf6b3689fb_css-design-awards-logos-id1L9L8Yvp%201.svg",
+            number: "52",
+        },
+        {
+            url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63d6e83420934a94d642103b_NCC2021_LogoLockup%201.svg",
+            number: "2",
+        }, 
+
     ]
   return (
     <div className='flex'>
