14d3a86c4a86f141dd007e26b10ce1b8ab8ec6f6
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-05 16:43:15 +0530

    updated stripe

--- components/Stripes.tsx
+++ components/Stripes.tsx
@@ -8,16 +8,17 @@ function Stripes() {
             number: "15",
         },
         {
-            url: "https://assets-global.website-files.com/6334198f23…3689fb_css-design-awards-logos-id1L9L8Yvp%201.svg",
+            url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63c9ce90a51cd7cf6b3689fb_css-design-awards-logos-id1L9L8Yvp%201.svg",
             number: "52",
         },
         {
             url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63d6e83420934a94d642103b_NCC2021_LogoLockup%201.svg",
             number: "2",
-        }, 
+        },
+         
     ]
   return (
-    <div>
+    <div className='flex'>
         {data.map((elem, index) => (
             <Stripe key={index} val={elem} />
         ))}
