4dfd63f10aa7ca41e5465d06cbc4d44331d10b60
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-05 15:46:38 +0530

    updated stripe

--- components/Stripe.tsx
+++ components/Stripe.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 function Stripe() {
   return (
-    <div className='max-w-72 px-'>
-      Hello
+    <div 
+        className='max-w-72 px-4 py-3 border-r-[1px] border-t-[1px] 
+    border-b-[1px] border-[#333] flex justify-between items-center'>
+      <span>image</span>
+      <span>43</span>
     </div>
   )
 }
