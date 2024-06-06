ccc9bc49dc084879ba9551de8894a44e025a920a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-06 22:29:01 +0530

    added

--- components/Marquees.tsx
+++ components/Marquees.tsx
@@ -20,7 +20,7 @@ function Marquees() {
              'https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d2754882567001f1ee80_spotify.svg'},
              'https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d275999da7719dc1fe2c_haufe.svg'},
             'https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d2759c8b021207af521b_yahoo.svg'},
-            {url: 'https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d27565c41bb608f7f7c0_rainfall.svg'},
+             'https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d27565c41bb608f7f7c0_rainfall.svg'},
             {url: 'https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d275ff2309d9d743ec75_doxel.svg'},
             {url: 'https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d2755ba681134c9d9b5c_mural.svg'},
             {url: 'https://assets-global.website-files.com/6334198f239547d0f9cd84b3/661b2455cb60f1919ab48e20_Logo.svg'}, 
