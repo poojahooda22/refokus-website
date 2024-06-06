0aace06b2f9fe909cbf73d3630925c5c3a431e74
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-06 15:51:44 +0530

    updated Marquee

--- components/Marquees.tsx
+++ components/Marquees.tsx
@@ -10,7 +10,8 @@ function Marquees() {
           {url: 'https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d275c2fcf540272672ef_intenseeye.svg'},
           {url: 'https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d27518bb52e4d642644e_bcgp.svg'},
           {url: 'https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d2756e8df24e52915c71_flowcode.svg'},
-          {url: 'https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d275e1d5aa08ccc379dd_ypo.svg'}
+          {url: 'https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d275e1d5aa08ccc379dd_ypo.svg'},
+          {url: ''}
         ],
         []
     ]
