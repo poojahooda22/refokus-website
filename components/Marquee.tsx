3a9023d007b92fd5ff51bcf704969a5842341b4a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-06 22:10:28 +0530

    updated marquee

--- components/Marquee.tsx
+++ components/Marquee.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Marquee({imagesUrl}: {imagesUrl: any}) {
     return (
-        <div>
+        <div className=''>
                 {imagesUrl.map((url: string) => <img  src={url} className='' alt=""/>)}
         </div>
     )
