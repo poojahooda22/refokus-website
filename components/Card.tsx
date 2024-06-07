c10c99e74a7f795073369934f88d0e15772b289c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-07 23:03:37 +0530

    added

--- components/Card.tsx
+++ components/Card.tsx
@@ -23,21 +23,13 @@ function Card({width, toptitle, toptitle2, start, para}:
                 <>
                     <h1 className='text-[5vw] font-medium leading-none'>Start a Project </h1>
                     <button className='text-[.8vw] px-[1.3vw] py-[.7vw] rounded-full mt-[1vw] border-[1px] border-secondary'>Contact us</button>
-                </>
-                
+                </>               
                 :  null
             }
             {
                 para === true ? <p className='text-[.7vw] text-gray3'>Explore what drives our team.</p>
                 :  null
-            }
-            {
-                button === true ? 
-                :  null
-            }
-            
-           
-            
+            }        
         </div>
     </div>
   )
