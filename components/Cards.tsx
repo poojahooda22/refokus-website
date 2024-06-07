da773a33b850f034fc549b884bc1ed7c1184ca39
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-07 14:48:58 +0530

    updated cards

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -4,7 +4,7 @@ import Card from './Card'
 function Cards() {
   return (
     <div className='w-full py-[4vw]'>
-        <div className='max-w-screen-xl mx-auto '>
+        <div className='max-w-screen-xl mx-auto flex gap-2 '>
             <Card />
         </div>
     </div>
