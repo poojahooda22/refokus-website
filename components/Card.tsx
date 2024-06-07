5a2fbd12c0f0a7faf1b1527ff679d543df862539
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-07 15:28:58 +0530

    updated card

--- components/Card.tsx
+++ components/Card.tsx
@@ -9,15 +9,18 @@ function Card({width}: {width: string}) {
                 <p className='text-[.7vw]'>Up Next: News</p>
                 <Image src='/arrow.svg' width={20} height={20} alt=""/>
             </div>
-            <div className='w-1/3'>
+            <div className='w-1/2'>
                 <h3 className=' font-medium text-[1.2vw]'>Insights and behind the scenes</h3>
             </div>
             
         </div> 
 
         <div className='down w-full mt-[8vw]'>
-            <h1 className='text-[5vw] font-medium'>Start a Project </h1>
-            <button className='text-[.8vw] px-[1.3vw] py-[.7vw] rounded-full mt-[1vw] border-[1px] border-secondary'>Contact us</button>
+            <h1 className='text-[5vw] font-medium leading-none'>Start a Project </h1>
+            <button className='text-[.8vw] px-[1.3vw] py-[.7vw] rounded-full mt-[1vw] border-[1px] border-secondary'>
+                Contact us
+            </button>
+            
         </div>
     </div>
   )
