1748132f114ed38ba68a76b74cfe6ea537ad95ea
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-07 15:16:41 +0530

    updated card

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,14 +1,16 @@
+import Image from 'next/image'
 import React from 'react'
 
-function Card({width}) {
+function Card({width}: {width: string}) {
   return (
     <div className={`${width} h-[10vw] bg-gray rounded-lg py-[1vw] px-[1vw]`}>
-      <div className='flex items-center justify-between mb-[1vw]'>
-        <p>Up Next: News</p>
-        <img src="/arrow.svg" alt="Project Image" className='' />
-      </div>
-        <h3>Insights and behind the scenes</h3> 
-      
+        <div className='w-full'>
+            <div className='flex items-center justify-between mb-[1vw]'>
+                <p className='text-[.7vw]'>Up Next: News</p>
+                <Image src='/arrow.svg' width={20} height={20} alt=""/>
+            </div>
+            <h3>Insights and behind the scenes</h3>
+        </div> 
     </div>
   )
 }
