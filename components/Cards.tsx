611fdf3df28b57b78ae029e0eaabcef4125e818c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-07 15:57:27 +0530

    updated cards

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -5,7 +5,8 @@ function Cards() {
   return (
     <div className='w-full py-[4vw]'>
         <div className='max-w-screen-xl mx-auto flex gap-4 '>
-            <Card width="w-1/3" toptitle='Up Next: News' toptitle2='Insights and behind the scenes' />
+            <Card width="w-1/3" 
+                toptitle='Up Next: News' toptitle2='Insights and behind the scenes' />
             <Card width="w-2/3" toptitle='Get In Touch' toptitle2="Let's get to it, together."/>
         </div>
     </div>
