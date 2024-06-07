d5b889ee319131a7f1928efa57b1884058e2f837
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-07 23:05:49 +0530

    added

--- components/Card.tsx
+++ components/Card.tsx
@@ -17,12 +17,12 @@ function Card({width, toptitle, toptitle2, start, para}:
             
         </div> 
 
-        <div className='down w-full mt-[8vw]'>
+        <div className='down w-full'>
             {
                 start === true ? 
                     <>
                         <h1 className='text-[5vw] font-medium leading-none'>Start a Project </h1>
-                        <button className='text-[.8vw] px-[1.3vw] py-[.7vw] rounded-full mt-[1vw] border-[1px] border-secondary'>Contact us</button>
+                        <button className='text-[.8vw] px-[1.3vw] py-[.5vw] rounded-full mt-[1vw] border-[1px] border-secondary'>Contact us</button>
                     </>               
                 :  null
             }
