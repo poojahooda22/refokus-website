22c6839ab16d00154112c9d4097443baacf57b6d
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-07 14:59:13 +0530

    updated cards

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -5,7 +5,8 @@ function Cards() {
   return (
     <div className='w-full py-[4vw]'>
         <div className='max-w-screen-xl mx-auto flex gap-2 '>
-            <Card />
+            <Card width="w-1/3" />
+            <Card width="w-1/2"/>
         </div>
     </div>
   )
