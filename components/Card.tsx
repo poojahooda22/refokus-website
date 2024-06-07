b5e66f4cd050cae8ea6c66784fb3e68b74d3a4eb
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-07 15:32:38 +0530

    updated card

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,16 +1,16 @@
 import Image from 'next/image'
 import React from 'react'
 
-function Card({width}: {width: string}) {
+function Card({width, toptitle, toptitle2}: {width: string, toptitle: string, toptitle2?: string}) {
   return (
     <div className={`${width}  bg-gray rounded-lg py-[1vw] px-[1vw]`}>
         <div className='w-full'>
             <div className='flex items-center justify-between mb-[1vw]'>
-                <p className='text-[.7vw]'>Up Next: News</p>
+                <p className='text-[.7vw]'>{toptitle}</p>
                 <Image src='/arrow.svg' width={20} height={20} alt=""/>
             </div>
             <div className='w-1/2'>
-                <h3 className=' font-medium text-[1.2vw]'>Insights and behind the scenes</h3>
+                <h3 className=' font-regular text-[1.2vw] '>{toptitle2}</h3>
             </div>
             
         </div> 
@@ -20,7 +20,7 @@ function Card({width}: {width: string}) {
             <button className='text-[.8vw] px-[1.3vw] py-[.7vw] rounded-full mt-[1vw] border-[1px] border-secondary'>
                 Contact us
             </button>
-            
+
         </div>
     </div>
   )
