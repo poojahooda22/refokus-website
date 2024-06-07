36c441ec2f95696bcd009cb2f196ba319bd3d555
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-07 15:25:28 +0530

    updated card

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,17 +3,21 @@ import React from 'react'
 
 function Card({width}: {width: string}) {
   return (
-    <div className={`${width} h-[10vw] bg-gray rounded-lg py-[1vw] px-[1vw]`}>
+    <div className={`${width}  bg-gray rounded-lg py-[1vw] px-[1vw]`}>
         <div className='w-full'>
             <div className='flex items-center justify-between mb-[1vw]'>
                 <p className='text-[.7vw]'>Up Next: News</p>
                 <Image src='/arrow.svg' width={20} height={20} alt=""/>
             </div>
-            <h3>Insights and behind the scenes</h3>
+            <div className='w-1/3'>
+                <h3 className=' font-medium text-[1.2vw]'>Insights and behind the scenes</h3>
+            </div>
+            
         </div> 
 
-        <div>
-            <p>Explore </p>
+        <div className='down w-full mt-[8vw]'>
+            <h1 className='text-[5vw] font-medium'>Start a Project </h1>
+            <button className='text-[.8vw] px-[1.3vw] py-[.7vw] rounded-full mt-[1vw] border-[1px] border-secondary'>Contact us</button>
         </div>
     </div>
   )
