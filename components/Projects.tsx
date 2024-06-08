dc6656321cfa93019152d2f4716a92ce1a5bd688
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-08 11:46:54 +0530

    updated projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -4,7 +4,9 @@ import Project from './Project'
 function Projects() {
   return (
     <div className='w-full py-[4vw]'>
-      <Project />
+        <div>
+            <Project />
+        </div>
     </div>
   )
 }
