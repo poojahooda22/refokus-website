5e1a286b8109cf1926e2fc0b7dbd8f35081b3e3e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-08 13:08:57 +0530

    updated projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -5,17 +5,28 @@ function Projects() {
     const data =[
         {
             image1: 'https://assets-global.website-files.com/6334198f239547f2fccd84c1/650c5f12d26da475b2fd6b88_15.webp',
-            image2: '',
+            image2: 'https://assets-global.website-files.com/6334198f239547f2fccd84c1/650c5f15978b05dc7f2ae554_14.webp',
             logo: 'https://assets-global.website-files.com/6334198f239547f2fccd84c1/64c1cdf5425388ed4532f603_Arqitel%20Logo%20White.svg',
+            videoUrl: 'https://cdn.refokus.com/website/Arqitel/Arqitel%20project%20video%204_3.webm',
+            desc: 'Concept, Design, 3D, Webflow+WebGL Development',
+            heading: "With a continuous 3D animation, we showcase Arqitel approach and show how migration data translates into real estate."
+        },
+        {
+            image1: '',
+            image2: '',
+            logo: '',
             videoUrl: '',
-            desc: '',
-            heading: ''
-        }
+            desc: 'Concept, Design, 3D, Webflow+WebGL Development',
+            heading: "With a continuous 3D animation, we showcase Arqitel approach and show how migration data translates into real estate."
+        },
+
     ]
   return (
     <div className='w-full py-[4vw]'>
         <div className='max-w-screen-xl mx-auto '>
-            <Project />
+            {data.map((elem, index) => (
+                <Project key={index} val={elem} />
+            ))}
         </div>
     </div>
   )
