91ead01fecfd0c1e9c9a3982ee32b5b2918f4cb1
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-08 15:32:56 +0530

    updated button

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -3,14 +3,14 @@ import React from 'react'
 
 function Button({text}: {text: string}) {
   return (
-    <div className='w-fit px-[1vw] py-[.3vw]  bg-secondary rounded-full flex items-center gap-[.5vw] justify-center'>
-      <div className='h-[1.5vw] ctaWrapper overflow-hidden text-primary '>
-        <h5 className='text-[.8vw] leading-[1.4vw]  '>{text}</h5>
-        <h5 className='text-[.8vw] leading-[1.4vw]'>{text}</h5>
+    <div className='w-fit px-[6vw] py-[3vw] sm:px-[1vw] sm:py-[.3vw]  bg-secondary rounded-full flex items-center gap-[.5vw] justify-center'>
+      <div className='h-[4vw] sm:h-[1.5vw] ctaWrapper overflow-hidden text-primary '>
+        <h5 className='text-[3.5vw] leading-[4vw] sm:text-[.8vw] sm:leading-[1.4vw]  '>{text}</h5>
+        <h5 className='text-[3.5vw] leading-[4vw] sm:text-[.8vw] sm:leading-[1.4vw]'>{text}</h5>
       </div>
-      <div className='h-[.5vw] ctaIcon overflow-hidden'>
-        <Image src='/icon.svg' width={8} height={8} alt="" className='w-[.9vw]' />
-        <Image src='/icon.svg' width={8} height={8} alt="" className='w-[.9vw]' />
+      <div className='h-[1.9vw] sm:h-[.5vw] ctaIcon overflow-hidden'>
+        <Image src='/icon.svg' width={8} height={8} alt="" className='w-[3.2vw] sm:w-[.9vw]' />
+        <Image src='/icon.svg' width={8} height={8} alt="" className='w-[3.2vw] sm:w-[.9vw]' />
       </div>
     </div>
   )
