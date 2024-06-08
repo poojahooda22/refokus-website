31fef2c9220f71938de90a26dc1bcf63fbb585ab
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-08 20:18:41 +0530

    updated button

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 function Button({text}: {text: string}) {
   return (
     <div className='w-fit px-[6vw] py-[3vw] sm:px-[1vw] sm:py-[.3vw]  bg-secondary rounded-full flex items-center gap-[.5vw] justify-center'>
-      <div className='h-[4vw] sm:h-[1.5vw] ctaWrapper overflow-hidden text-primary '>
+      <div className='h-[4vw] sm:h-[1.5vw] ctaWrapper overflow-hidden text-primary'>
         <h5 className='text-[3.5vw] leading-[4vw] sm:text-[.8vw] sm:leading-[1.4vw]'>{text}</h5>
         <h5 className='text-[3.5vw] leading-[4vw] sm:text-[.8vw] sm:leading-[1.4vw]'>{text}</h5>
       </div>
