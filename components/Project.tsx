9e2262317da5ecb94ab2ed06b1d65d75394f1f7f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-08 15:33:16 +0530

    updated peoject

--- components/Project.tsx
+++ components/Project.tsx
@@ -4,15 +4,15 @@ import Button from './button/Button'
 
 function Project({val}: {val: any}) {
   return (
-    <div className={`w-full py-[2vw] px-[2vw] rounded-2xl flex gap-[1vw]`} style={{ backgroundColor: val.bg }}>
-        <div className='w-1/2 flex flex-col items-start gap-[1.5vw] '>
+    <div className={`w-full sm:py-[2vw] py-[4vw] px-[4vw] sm:px-[2vw] rounded-2xl flex gap-[1vw]`} style={{ backgroundColor: val.bg }}>
+        <div className='sm:w-1/2 flex flex-col items-start gap-[1.5vw] '>
             <h2 className=''>
                 <Image src={val.logo}
                     alt="" width={32} height={32}
-                    className='w-[8vw]'    
+                    className='hidden sm:block sm:w-[8vw]'    
                 />
             </h2>
-            <div className='w-full flex justify-end'>
+            <div className='w-full hidden sm:flex justify-end'>
                 <div className='pt-[1vw]'>
                     <Image src={val.image1} alt="" 
                     width={1000} height={100}
@@ -22,10 +22,16 @@ function Project({val}: {val: any}) {
             </div> 
             <Image src={val.image2} alt="" 
             width={1000} height={32}
-            className='w-[38vw] rounded-2xl' />
+            className='w-[38vw] hidden sm:block rounded-2xl' />
         </div>
-        <div className='w-1/2'>
-            <div className='bg-secondary w-full h-[26vw] rounded-2xl overflow-hidden'>
+        <div className='w-full sm:w-1/2'>
+            <h2 className=''>
+                <Image src={val.logo}
+                    alt="" width={32} height={32}
+                    className=' sm:hidden w-[32vw] py-[4vw] mb-[3vw]'    
+                />
+            </h2>
+            <div className='bg-secondary w-full sm:h-[26vw] rounded-2xl overflow-hidden'>
                 <video src={val.videoUrl}
                     autoPlay
                     muted
@@ -33,9 +39,9 @@ function Project({val}: {val: any}) {
                     className=''
                 />
             </div>
-            <div className=' mt-[2vw] pl-[1vw]'>
-                <p className='text-[.8vw] mb-[.7vw] tracking-tight'>{val.desc}</p>
-                <h3 className='text-[1.1vw] leading-[1.4vw] mb-[2vw] tracking-tight'>{val.heading}</h3>
+            <div className='mt-[6vw] sm:mt-[2vw] pl-[1vw]'>
+                <p className='text-[3vw] sm:text-[.8vw] mb-[2vw] sm:mb-[.7vw] tracking-tight'>{val.desc}</p>
+                <h3 className='text-[4vw] leading-[5vw] sm:text-[1.1vw] sm:leading-[1.4vw] mb-[8vw] sm:mb-[2vw] tracking-tight'>{val.heading}</h3>
                 <Button text='View Website' />
             </div>
         </div>
