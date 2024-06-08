44908f413049f9986b091bdec14727e2f83de90a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-08 20:18:17 +0530

    updated button

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -5,7 +5,7 @@ function Button({text}: {text: string}) {
   return (
     <div className='w-fit px-[6vw] py-[3vw] sm:px-[1vw] sm:py-[.3vw]  bg-secondary rounded-full flex items-center gap-[.5vw] justify-center'>
       <div className='h-[4vw] sm:h-[1.5vw] ctaWrapper overflow-hidden text-primary '>
-        <h5 className='text-[3.5vw] leading-[4vw] sm:text-[.8vw] sm:leading-[1.4vw]  '>{text}</h5>
+        <h5 className='text-[3.5vw] leading-[4vw] sm:text-[.8vw] sm:leading-[1.4vw] '>{text}</h5>
         <h5 className='text-[3.5vw] leading-[4vw] sm:text-[.8vw] sm:leading-[1.4vw]'>{text}</h5>
       </div>
       <div className='h-[1.9vw] sm:h-[.5vw] ctaIcon overflow-hidden'>
