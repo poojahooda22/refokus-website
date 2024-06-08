6f1e6e1eebb69848f0719c59ee2b79c0c54c4c57
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-08 11:45:58 +0530

    updated stripes

--- components/Stripes.tsx
+++ components/Stripes.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Stripe from './Stripe'
 
-function Stripes() {
+function Stripes({padding}: {padding: string}) {
     var data = [
         {
             url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63c9ce90a51cd75d563689fd_Awwards-logotype-2018%201.svg",
@@ -30,7 +30,7 @@ function Stripes() {
 
     ]
   return (
-    <div className=' '>
+    <div className={`${padding}`}>
         <p 
               className=' sm:hidden text-[4.2vw]  text-center
               sm:text-[1.5vw] xl:text-[1vw] font-regular text-[#787878] pt-[12vw]'
