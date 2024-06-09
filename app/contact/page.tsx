374394ebf47d4b46a316e936ba985528cf03e258
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-09 07:41:32 +0530

    updated page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+'use client'
 
-function page() {
+import React from 'react'
+import Cursor from "@/components/StickyCursor/Index";
+import { useRef} from 'react';
+import Header from '@/components/header/Index'
+import ContactForm from '@/components/ContactPage';
+
+function Contact() {
+    const stickyElement = useRef(null);
   return (
-    <div>
-      
+    <div  className='w-full min-h-[100vh] relative bg-primary text-secondary overflow-hidden'>
+       <Cursor stickyElement={stickyElement} />
+       <Header/>
+       <ContactForm/>
     </div>
   )
 }
 
-export default page
+export default Contact
