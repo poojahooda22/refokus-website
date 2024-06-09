fb6de8d9643622a92b9c87090da7126c9bb1a5af
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-09 07:42:55 +0530

    updated index

--- components/header/Index.tsx
+++ components/header/Index.tsx
@@ -41,7 +41,8 @@ const Header = forwardRef(function Index(props, ref){
         <div className='w-screen flex  justify-center'>
             <div 
                 className={`${styles.header} max-w-screen-xl mx-auto py-[2vw] 
-                    bg-primary px-[5vw] sm:py-[1vw] sm:px-[5vw] lg:px-[5vw] xl:px-0`} 
+                    bg-primary px-[5vw] sm:py-[1vw] sm:px-[5vw] lg:px-[5vw] xl:px-0`
+                } 
                 style={{ overflow: "hidden"}}
             >
                 <div className='flex items-center gap-[4vw]'>
