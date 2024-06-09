ddf5d0db5c136aa5b05f26691239936378cfc0c4
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-09 07:49:21 +0530

    added

--- components/Card.tsx
+++ components/Card.tsx
@@ -38,7 +38,8 @@ function Card({width, toptitle, toptitle2, start, para}:
             }
             {
                 para &&  
-                <p className='text-[3vw] sm:text-[.7vw] text-gray3'>Explore what drives our team.</p>
+                <p className='text-[3vw] sm:text-[.7vw] text-gray3'>
+                    Explore what drives our team.</p>
                   
             }        
         </div>
