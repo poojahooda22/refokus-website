f06d8848af057d64ce1fc2b3415a4dd998b64c36
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-09 07:54:10 +0530

    added

--- components/Product.tsx
+++ components/Product.tsx
@@ -7,7 +7,7 @@ function Product({val}: {val: any}) {
       <div className='max-w-screen-xl mx-auto flex items-center justify-between py-[2vw]'>
        <h2 className='text-[2vw] font-medium'>{val.title}</h2>
        <div className='w-1/3 flex flex-col gap-[1vw]'>
-            <p className='w-3/4 text-[.9vw] leading-[1.5vw]'>
+          <p className='w-3/4 text-[.9vw] leading-[1.5vw]'>
               {val.desc}
             </p>
             <div className='flex items-center gap-[1vw]'>
