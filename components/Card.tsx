9ddcfb5925009cd4d6d1d98cfd9c74d628836b7b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-09 07:50:07 +0530

    added

--- components/Card.tsx
+++ components/Card.tsx
@@ -30,7 +30,8 @@ function Card({width, toptitle, toptitle2, start, para}:
                     <>
                         <h1 className='text-[12vw] sm:text-[5vw] font-medium leading-none'>Start a Project </h1>
                         <Link href="/contact">
-                            <button className='text-[3vw] sm:text-[.8vw] py-[3vw] px-[5vw] sm:px-[1.3vw] mt-[4vw]
+                            <button 
+                            className='text-[3vw] sm:text-[.8vw] py-[3vw] px-[5vw] sm:px-[1.3vw] mt-[4vw]
                             sm:py-[.5vw] rounded-full sm:mt-[1vw] border-[1px] border-secondary'>Contact us</button>
                         </Link>
                     </>               
