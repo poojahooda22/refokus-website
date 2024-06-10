33225a763607cce0356438ba863e94107088d383
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-10 21:25:39 +0530

    updated

--- components/Product.tsx
+++ components/Product.tsx
@@ -4,8 +4,10 @@ import Button from './button/Button'
 function Product({val}: {val: any}) {
   return (
     <div 
-      className={`w-full py-5 h-[16vw] transition-transform`}
-      
+      className={`w-full py-5 h-[16vw] transition-transform `}
+      style={{ transition: 'background-color 0.3s ease'  }}
+      onMouseEnter={(e) => e.currentTarget.style.backgroundColor = val.bg}
+      onMouseLeave={(e) => e.currentTarget.style.backgroundColor = 'initial'}
     >
       <div className='prodtAnim max-w-screen-xl mx-auto  flex items-center justify-between py-[2vw]'>
        <h2 className='text-[2vw] font-medium'>{val.title}</h2>
