af3fc1ef20545e403ea40b981677e0a15f92c269
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-10 23:08:44 +0530

    added

--- components/Card.tsx
+++ components/Card.tsx
@@ -29,7 +29,7 @@ function Card({width, toptitle, toptitle2, start, para}:
                 start &&  
                 <>
                     <h1 
-                            className='text-[12vw] sm:text-[5vw] 
+                        className='text-[12vw] sm:text-[5vw] 
                             font-medium leading-none'
                         >
                             Start a Project 
