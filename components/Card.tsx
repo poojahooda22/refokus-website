4afe0a223481730f773f9ce18a984e886c9b0e77
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-10 23:09:57 +0530

    added

--- components/Card.tsx
+++ components/Card.tsx
@@ -39,7 +39,7 @@ function Card({width, toptitle, toptitle2, start, para}:
                             className='text-[3vw] sm:text-[.8vw]
                                 py-[3vw] px-[5vw] sm:px-[1.3vw] mt-[4vw]
                                 sm:py-[.5vw] rounded-full sm:mt-[1vw] 
-                                border-[1px] border-secondary'
+                            border-[1px] border-secondary'
                         >
                             Contact us
                         </button>
