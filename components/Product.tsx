412bf928fb6b85e2442bc0c7ef8ee11cbd4056b3
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-10 21:14:10 +0530

    updated

--- components/Product.tsx
+++ components/Product.tsx
@@ -3,8 +3,8 @@ import Button from './button/Button'
 
 function Product({val}: {val: any}) {
   return (
-    <div className='w-full py-5'>
-      <div className='max-w-screen-xl mx-auto flex items-center justify-between py-[2vw]'>
+    <div className='w-full py-5 h-[16vw] hover:bg-comp transition-transform'>
+      <div className='prodtAnim max-w-screen-xl mx-auto  flex items-center justify-between py-[2vw]'>
        <h2 className='text-[2vw] font-medium'>{val.title}</h2>
        <div className='w-1/3 flex flex-col gap-[1vw]'>
           <p className='w-3/4 text-[.9vw] leading-[1.5vw]'>
