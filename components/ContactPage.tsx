2a9ea62aa6654b74525a754c863cce9ccbb593cd
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-10 14:30:28 +0530

    updated button

--- components/ContactPage.tsx
+++ components/ContactPage.tsx
@@ -4,11 +4,22 @@ function ContactForm() {
   return (
     <div className='w-full relative'>
       <div className='max-w-screen-xl mx-auto pt-[8vw]'>
-        <div className='w-1/2'>
+        <div className='w-1/2 min-h-[20vw] flex flex-col justify-between'>
           <h1 className='text-[5vw] uppercase leading-none'>Don't Be Shy with us</h1>
-          <p >
-            Don’t like forms? 
-          </p>
+          <div>
+            <p>
+              Don’t like forms? Well, too bad! This one is connected to our CRM and makes everything 
+              easier. Ok ok... here we go:
+            </p>
+            <button 
+              className='text-[3vw] sm:text-[.8vw]
+              py-[3vw] px-[5vw] sm:px-[1.3vw] mt-[4vw]
+              sm:py-[.5vw] rounded-full sm:mt-[1vw] 
+              border-[1px] border-secondary'
+            >
+                View more projects
+            </button>
+          </div>  
         </div>
         
       </div>
