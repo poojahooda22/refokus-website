fb0d436d4c1ecc57012f57e0c5a9c9d67481e289
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-10 14:46:12 +0530

    updated project

--- components/Project.tsx
+++ components/Project.tsx
@@ -4,6 +4,14 @@ import Button from './button/Button'
 import { useScroll, useTransform , motion} from 'framer-motion';
 
 function Project({val}: {val: any}) {
+    const ref = useRef<HTMLDivElement>(null);
+    const { scrollYProgress} = useScroll ({
+        target: ref,
+        offset: ["0 1", "1.33 1"],
+    });
+
+    const scaleProgress = useTransform(scrollYProgress, [0,1], [0.8, 1]);
+    const opacityProgress = useTransform(scrollYProgress, [0,1], [0.6, 1]);
   return (
     <div 
         className={`w-full sm:py-[2vw] py-[4vw] 
