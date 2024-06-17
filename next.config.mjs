7947dd45ef820d57d7c8c28030c8160d5257d86c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-17 23:20:22 +0530

    added

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-    images: {
+  images: {
       domains: ['assets-global.website-files.com'],
     }, 
 };
