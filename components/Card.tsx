be239eb0b0cdc01c2955bbc602b4374da73099d8
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-18 20:40:17 +0530

    updated card

--- components/Card.tsx
+++ components/Card.tsx
@@ -22,7 +22,7 @@ function Card({width, toptitle, toptitle2, start, para}:
                 <p 
                     className='text-[3vw] 
                     sm:text-[.7vw]'
-                    >
+                >
                     {toptitle}
                 </p>
                 <Image src='/arrow.svg' 
