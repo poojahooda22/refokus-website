102c4b6035758cd9d105d9c4e269adf284efba2b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-18 20:44:12 +0530

    added

--- components/Faq.tsx
+++ components/Faq.tsx
@@ -39,7 +39,8 @@ function Faq() {
                         {item.title}
                     </h4>
                     <span 
-                        className='text-[2vw] text-gray2'>
+                        className='text-[2vw] 
+                        text-gray2'>
                         {expand ? '-' : '+'}
                     </span>
                 </div>            
