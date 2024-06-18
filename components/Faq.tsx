bcec53624ebc5d68cb9458cd13b727b55e85b97f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-18 20:42:38 +0530

    added

--- components/Faq.tsx
+++ components/Faq.tsx
@@ -45,7 +45,8 @@ function Faq() {
                 { 
                     expand && 
                     <div 
-                        className='text-[.8vw] font-normal'>
+                        className='text-[.8vw] 
+                        font-normal'>
                         {item.desc}
                     </div>}    
             </div>
