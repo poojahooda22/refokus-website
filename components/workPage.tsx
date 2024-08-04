1ce4b6aaaa2e084260a22ebeb8c1eeeef1a9e769
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-04 20:32:49 +0530

    updated work page

--- components/workPage.tsx
+++ components/workPage.tsx
@@ -84,7 +84,7 @@ function WorkPage() {
               -rotate-90 font-medium leading-none absolute left-1/3'
             >
               work
-              </h1>
+            </h1>
             <h1 className='sm:hidden text-[52vw] lg:text-[28vw] -rotate-90 font-medium leading-none absolute -left-1/2 '>work</h1>
           </div>
             
