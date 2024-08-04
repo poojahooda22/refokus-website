4c0fbacb7071d77d320231d104781fafdb2afc61
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-04 20:34:16 +0530

    added

--- components/workPage.tsx
+++ components/workPage.tsx
@@ -88,7 +88,7 @@ function WorkPage() {
             <h1 className='sm:hidden text-[52vw] lg:text-[28vw] 
               -rotate-90 font-medium leading-none 
               absolute -left-1/2'
-              >
+            >
               work</h1>
           </div>
             
